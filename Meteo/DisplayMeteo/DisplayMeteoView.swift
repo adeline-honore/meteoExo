ebb4b650f20444bbe43adfc6b863752c2a0e3a2a
adeline-honore <honore_adeline@yahoo.fr>
2022-12-17 23:40:58 +0100

    creation de DisplayMeteoViewController et de DisplayMeteoView

---
+++ Meteo/DisplayMeteo/DisplayMeteoView.swift
@@ -0,0 +1,15 @@
+//
+//  DisplayMeteoView.swift
+//  Meteo
+//
+//  Created by HONORE Adeline on 17/12/2022.
+//
+
+import UIKit
+
+class DisplayMeteoView: UIView {
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var startAgainButton: UIButton!
+}
---
+++ Meteo/DisplayMeteo/DisplayMeteoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DisplayMeteoViewController.swift
+//  Meteo
+//
+//  Created by HONORE Adeline on 17/12/2022.
+//
+
+import UIKit
+
+class DisplayMeteoViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
